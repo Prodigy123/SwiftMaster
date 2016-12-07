f63d19818006f1f6f9fec48ec19c3b19ffe43892
George <jacontact123@163.com>
2016-12-07 20:04:14 +0800

    Timer

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     @objc private func updateTime(){
         displayNum = displayNum! + 0.1
